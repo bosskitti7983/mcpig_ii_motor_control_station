ad866b3474f567e8ea6ffbe904eed048d72d7779
bosskitti7983 <bosskitti7983@gmail.com>
2022-07-31 18:49:06 +0700

    update velocity_control code

--- scripts/velocity_control.py
+++ scripts/velocity_control.py
@@ -4,15 +4,21 @@ from sys import exit
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import JointState
+from math import pi
 
+wheel_size_mm = 42
+max_rpm = 70
+limit_speed_rpm = lambda input_rpm : min(max( int(input_rpm)/60*2*pi , -max_rpm/60*2*pi), max_rpm/60*2*pi)   
+# speed =  1/.229  ==> 41rpm / 2/3 rps 
 
+speed = lambda input : 70/60*2*pi    
 
-speed =  70/60 * 0.9
-speed = min(max(speed, -7.15) , 7.15)
+# speed = min(max(speed, -limit_speed) , limit_speed)
 
 def talker():
     motors_num = 1
 
+    
     pub = rospy.Publisher('/db_dynamixel_ROS_driver/hexapod_state_commands', JointState, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(30) # 10hz
@@ -31,10 +37,12 @@ def talker():
     # o2 = 0.1
 
     while not rospy.is_shutdown():
+   
+        input_rad_p_s = limit_speed_rpm(input('input rpm(max is 70)'))
+        rospy.loginfo("set speed {rpm:.2f} rpm, (approx) {mmps:.2f} mm/s".format(rpm=input_rad_p_s*60/(2*pi), mmps=(input_rad_p_s/(2*pi)*pi*wheel_size_mm) ))
 
-        
 
-        joint_state.velocity[0] = speed
+        joint_state.velocity[0] = input_rad_p_s
         # joint_state.position[0] = 0.5
 
         hello_str = "hello world %s" % rospy.get_time()
